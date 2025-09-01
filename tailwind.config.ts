f2ef22acde16c87d9ed925c25a61bc97ce488593
Ronald E. Aybar D <read424@gmail.com>
2025-09-01 06:16:45 -0500

    chore(deps): actualizar dependencias y configuración de tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,6 @@ import type { Config } from 'tailwindcss'
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   darkMode: 'class', // Habilita el modo oscuro usando la clase 'dark'
